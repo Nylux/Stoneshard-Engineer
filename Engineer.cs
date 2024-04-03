4617accbefb6477f52b6222a4d250c4dca72ebb2
Nylux <nylux42@gmail.com>
2024-04-03 14:32:15 +0200

    Initial commit

---
+++ Engineer.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C)
+// See LICENSE file for extended copyright information.
+// This file is part of the repository from .
+
+using ModShardLauncher;
+using ModShardLauncher.Mods;
+
+namespace Engineer;
+
+public class Engineer : Mod
+{
+    public override string Author => "Nylux";
+    public override string Name => "Engineer Class";
+    public override string Description => "A content mod that adds a class focused around positionning and traps.";
+    public override string Version => "1.0.0.0";
+    public override string TargetVersion => "0.8.2.10";
+
+    public override void PatchMod()
+    {
+        
+    }
+}
